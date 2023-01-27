6db3a5896e1793cbed6f2e2115f3c5f2170cebe1
lpyNeil <lpyneil@outlook.com>
2023-01-27 19:32:24 +0800

    Some modifications.

--- Function.h
+++ Function.h
@@ -5,68 +5,68 @@
 #include <conio.h>
 extern int rows;
 namespace Function {
-    inline void cls() {
-        system("cls");
-        return;
-    }
-    inline void goto_xy(HANDLE hOut, int x, int y) {
-        COORD pos{};
-        pos.X = x;
-        pos.Y = y;
-        SetConsoleCursorPosition(hOut, pos);
-        return;
-    }
-    inline void middle(std::string str) {
-        HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_SCREEN_BUFFER_INFO bInfo;
-        GetConsoleScreenBufferInfo(hOutput, &bInfo);
-        int dwSizeX = bInfo.dwSize.X, dwSizey = bInfo.dwSize.Y;
-        int len = int(str.length()) - 1;
-        int x = dwSizeX / 2 - len / 2;
-        goto_xy(hOutput, x, rows++);
-        std::cout << str;
-        if (str == "Press any key to continue . . .") return;
-        std::cout << std::endl;
-        return;
-    }
-    inline void hide_cursor() {
-        HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_CURSOR_INFO CursorInfo;
-        GetConsoleCursorInfo(handle, &CursorInfo);
-        CursorInfo.bVisible = false;
-        SetConsoleCursorInfo(handle, &CursorInfo);
-        return;
-    }
-    inline void show_cursor() {
-        HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_CURSOR_INFO CursorInfo;
-        GetConsoleCursorInfo(handle, &CursorInfo);
-        CursorInfo.bVisible = true;
-        SetConsoleCursorInfo(handle, &CursorInfo);
-        return;
-    }
-    inline void press() {
-        middle("Press any key to continue . . .");
-        while (true) {
-            if (_kbhit()) break;
-        }
-        return;
-    }
-    inline void set_title(std::string title) {
-        SetConsoleTitle(title.c_str());
-        return;
-    }
-    inline void erase_spaces(std::string& formula) {
-        std::string formula1;
-        int len = int(formula.size());
-        for (int i = 0; i < len; i++) {
-            if (formula[i] != ' ') formula1 += formula[i];
-        }
-        formula = formula1;
-        return;
-    }
-    inline void transfer_lower(std::string& formula) {
-        transform(formula.begin(), formula.end(), formula.begin(), ::tolower);
-        return;
-    }
+	inline void cls() {
+		system("cls");
+		return;
+	}
+	inline void goto_xy(HANDLE hOut, int x, int y) {
+		COORD pos{};
+		pos.X = x;
+		pos.Y = y;
+		SetConsoleCursorPosition(hOut, pos);
+		return;
+	}
+	inline void middle(std::string str) {
+		HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_SCREEN_BUFFER_INFO bInfo;
+		GetConsoleScreenBufferInfo(hOutput, &bInfo);
+		int dwSizeX = bInfo.dwSize.X, dwSizey = bInfo.dwSize.Y;
+		int len = int(str.length()) - 1;
+		int x = dwSizeX / 2 - len / 2;
+		goto_xy(hOutput, x, rows++);
+		std::cout << str;
+		if (str == "Press any key to continue . . .") return;
+		std::cout << std::endl;
+		return;
+	}
+	inline void hide_cursor() {
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_CURSOR_INFO CursorInfo;
+		GetConsoleCursorInfo(handle, &CursorInfo);
+		CursorInfo.bVisible = false;
+		SetConsoleCursorInfo(handle, &CursorInfo);
+		return;
+	}
+	inline void show_cursor() {
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_CURSOR_INFO CursorInfo;
+		GetConsoleCursorInfo(handle, &CursorInfo);
+		CursorInfo.bVisible = true;
+		SetConsoleCursorInfo(handle, &CursorInfo);
+		return;
+	}
+	inline void press() {
+		middle("Press any key to continue . . .");
+		while (true) {
+			if (_kbhit()) break;
+		}
+		return;
+	}
+	inline void set_title(std::string title) {
+		SetConsoleTitle(title.c_str());
+		return;
+	}
+	inline void erase_spaces(std::string& formula) {
+		std::string formula1;
+		int len = int(formula.size());
+		for (int i = 0; i < len; i++) {
+			if (formula[i] != ' ') formula1 += formula[i];
+		}
+		formula = formula1;
+		return;
+	}
+	inline void transfer_lower(std::string& formula) {
+		transform(formula.begin(), formula.end(), formula.begin(), ::tolower);
+		return;
+	}
 }
--- Users.h
+++ Users.h
@@ -6,49 +6,54 @@
 #include "Function.h"
 using namespace Function;
 const int char_num = 29;
+int type;
 char character[char_num] =
 { '1','2','3','4','5','6','7','8','9','0',
-  '+','-','*','/','=','!','%','^','(',')',
-  '[',']','{','}','.','<','>','x','y' };
+  '+','-','*','/','=','|','!','%','^','x',
+  '[',']','{','}','.','<','>','(',')'};
 class Calculator {
 public:
-    Calculator() {
-        set_title("Calculator");
-        input();
-    }
-    inline void input() {
-        std::string formula;
-        getline(std::cin, formula);
-        erase_spaces(formula);
-        transfer_lower(formula);
-        check(formula);
-        return;
-    }
-    inline void check(std::string formula) {
-        int len = int(formula.size());
-        bool flag = false, flag1 = false;
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < char_num; j++) {
-                if (formula[i] == character[j]) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                if (!flag1) {
-                    flag1 = true;
-                    std::cout << "Error:" << std::endl;
-                }
-                std::cout << "  \"" << formula[i] << "\"" << ":illegal character" << std::endl;
-            }
-        }
-        if (!flag) {
-            std::ofstream outf;
-            outf.open("Formula.txt");
-            outf << formula;
-            outf.close();
-            std::cout << "  The original formula has been output to the file." << std::endl;
-        }
-        return;
-    }
+	Calculator() {
+		set_title("Calculator");
+		input();
+	}
+	inline void input() {
+		std::string formula;
+		getline(std::cin, formula);
+		erase_spaces(formula);
+		transfer_lower(formula);
+		check(formula);
+		return;
+	}
+	inline void check(std::string formula) {
+		int len = int(formula.size());
+		bool flag = false, flag1 = false;
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < char_num; j++) {
+				if (i == len - 1 && formula[i] == '=') type = 1;
+				else if (i != len - 1 && (formula[i] == '<' || formula[i] == '>')) type = 2;
+				else if (i != len - 1 && formula[i] == '=') type = 3;
+				else 
+				if (formula[i] == character[j]) {
+					flag = true;
+					break;
+				}
+			}
+			if (!flag) {
+				if (!flag1) {
+					flag1 = true;
+					std::cout << "Error:" << std::endl;
+				}
+				std::cout << "  \"" << formula[i] << "\"" << ":illegal character" << std::endl;
+			}
+		}
+		if (!flag) {
+			std::ofstream outf;
+			outf.open("Formula.txt");
+			outf << formula;
+			outf.close();
+			std::cout << "  The original formula has been output to the file." << std::endl;
+		}
+		return;
+	}
 };
